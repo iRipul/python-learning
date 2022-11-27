766c48d7e3504e2b8847b5ce59a055dece08e416
Ripul Chhabra <ripulchhabra1990@gmail.com>
2022-11-27 13:32:20 +0530

    add function to find max bid

--- auction_program/app.py
+++ auction_program/app.py
@@ -8,6 +8,13 @@ bids_data_list = []
 
 should_continue = True
 
+def find_max_bid(bids_data): 
+    max_bid = bids_data[0]
+    for bid in bids_data:
+        if max_bid["price"] < bid["price"]:
+            max_bid = bid
+    print(f"The winner is {max_bid['name']} with highest bid ${max_bid['price']}")
+
 while should_continue:
     name = input("Enter your name\n").lower()
     bid_price = float(input("Enter your bid $"))
@@ -22,12 +29,7 @@ while should_continue:
 
     if ans == "no":
         should_continue = False
-        max_bid = bids_data_list[0]
-        for bid in bids_data_list:
-            if max_bid["price"] < bid["price"]:
-                max_bid = bid
-            
-        print(f"The winner is {max_bid['name']} with highest bid ${max_bid['price']}")
+        find_max_bid(bids_data_list)
     else:
         os.system('clear')
 
