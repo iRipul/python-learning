aead2443ee48fc6afad51a6ba54c569a24a48e16
Ripul Chhabra <ripulchhabra1990@gmail.com>
2022-12-25 23:44:23 +0530

    added error handling

--- nato_alphabet_project/main.py
+++ nato_alphabet_project/main.py
@@ -6,8 +6,18 @@ with open("nato_phonetic_alphabet.csv") as data:
 
 alpha_dict = {row.letter: row.code for (index, row) in alpha_data.iterrows()}
 
-name = input("Type your name: ")
+valid = False
 
-result = [alpha_dict[letter.upper()] for letter in name]
 
-print(result)
+def generate_phonetic():
+    name = input("Type your name: ")
+    try:
+        result = [alpha_dict[letter.upper()] for letter in name]
+    except KeyError:
+        print("Sorry, please enter alphabets only")
+        generate_phonetic()
+    else:
+        print(result)
+
+
+generate_phonetic()
