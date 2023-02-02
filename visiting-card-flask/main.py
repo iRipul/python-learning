23dc744be3321877e4149e752a05e1ff883a73db
Ripul Chhabra <ripulchhabra1990@gmail.com>
2023-02-02 16:03:13 +0530

    updated templates

--- visiting-card-flask/main.py
+++ visiting-card-flask/main.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template
-
+import datetime
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", data={
+        "name": "Ripul Chhabra",
+        "profession": "Software Engineer",
+        "year": datetime.datetime.now().year,
+        "twitter": "https://twitter.com/ripulchhabra",
+        "github": "https://github.com/iRipul"
+    })
 
 
 if __name__ == "__main__":
